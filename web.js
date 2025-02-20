aef316e06b0a4a99f296d5e03ca2e86cf9fd4fd4
luisasisco <softwareluisa12@gmail.com>
2025-02-20 11:42:24 -0500

    Se agrego un autocompletar los campos en editar

--- web.js
+++ web.js
@@ -1,35 +1,39 @@
 let resevation = [
     {
+        id: 2,
         Nroom: "5",
         name: "Sofia",
         roomType: "Matrimonial",
         price: "50.000",
     },
     {
+        id: 4,
         Nroom: "15",
         name: "Mariana",
         roomType: "Suite",
         price: "120.000",
     },
     {
+        id: 5,
         Nroom: "20",
         name: "Andy",
         roomType: "Individual",
         price: "40.000",
     },
     {
+        id: 6,
         Nroom: "25",
         name: "Valeria",
         roomType: "Familiar",
         price: "90.000",
     },
     {
+        id: 7,
         Nroom: "10",
         name: "Jhon",
         roomType: "Familiar",
         price: "90.000",
     }
-
 ];
 
 let id = null;
@@ -45,8 +49,8 @@ const showData = () => {
               <td scope="row">${data.roomType}</td>
               <td scope="row">${data.price}</td>
               <td>
-                <button class="btn btn-primary btn-sm" onclick="openModalById(${data.Nroom})">Editar</button>
-                <button class="btn btn-danger btn-sm" onclick="delate(${data.Nroom})">Eliminar</button>
+                <button class="btn btn-primary btn-sm" onclick="openModalById(${data.id})">Editar</button>
+                <button class="btn btn-danger btn-sm" onclick="delate(${data.id})">Eliminar</button>
             </tr>`;
     });
 
@@ -55,27 +59,33 @@ const showData = () => {
     return table.innerHTML = informationData;
 }
 
-function openModalById(idParam = null){
-    if(idParam != null){
-        const resevations = resevation.find((r) => r.Nroom == idParam);
+function openModalById(idParam = null) {
+    if (idParam != null) {
         id = idParam;
-        document.getElementById("name").value = resevations.name; 
-        document.getElementById("precio").value = resevations.price;
-        document.getElementById("Habitacion").value = resevations.Habitacion;
-        document.getElementById("NHabitaciones").value = resevations.NHabitaciones;
-    }else{
+        const data = resevation.find((r) => r.id == idParam);
+        document.getElementById("name").value = data.name;
+        document.getElementById("precio").value = data.price;
+        document.getElementById("Habitacion").value = data.roomType;
+        document.getElementById("NHabitaciones").value = data.Nroom;
+    } else {
         id = null;
-        document.getElementById("name").value = ""; 
-        document.getElementById("precio").value = ""; 
-        document.getElementById("Habitacion").value =""; 
-        document.getElementById("NHabitaciones").value = ""; 
+        document.getElementById("name").value = "";
+        document.getElementById("precio").value = "";
+        document.getElementById("Habitacion").value = "";
+        document.getElementById("NHabitaciones").value = "";
     }
-   
+
     modal.show();
 }
 
+function saveValues(event) {
+    event.preventDefault();
+    modal.hide();
+}
+
 function delate(idpara) {
-    resevation=resevation.filter((r) => r.Nroom != idpara);
+    resevation = resevation.filter((r) => r.id != idpara);
     showData();
 }
+
 showData();
